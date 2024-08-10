92127b8f79547d5383fe8a0becd77fe31931b1bf
DuyThinh1415 <duythinh141592@gmail.com>
2024-08-10 11:20:56 +0700

    add code for 3d draw

--- FormatedCode/burningTest.py
+++ FormatedCode/burningTest.py
@@ -7,9 +7,17 @@ import threading
 from Libary.RunSequent import RunSimulationThread
 from Libary.function import *
 
+# def Process(idx):
+#     while True:
+#         param = np.random.rand(11)
+#         RunSimulationThread(idx,param)
+#         printWithTime(" Idx "+str(idx)+" done")
+
 def Process(idx):
     while True:
-        param = np.random.rand(11)
+        param = [0.84469084, 1, 0.30963334, 0.55527287, 0.43204053, 1, 1, 0.13892581, 0, 0.9941866,  1]
+        param[0] = np.random.rand() #Random K1
+        param[10] = np.random.rand() #Random E
         RunSimulationThread(idx,param)
         printWithTime(" Idx "+str(idx)+" done")
        
